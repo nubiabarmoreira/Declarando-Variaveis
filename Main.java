41127ab12a5a2300f41184adac8e33e3f940ffc7
Nubia Barcellar Moreira <nubia.moreira@zup.com.br>
2024-12-03 11:22:05 -0300

    feat: corrije código

--- Main.java
+++
@@ -1,6 +0,0 @@
-String nome = "Núbia";
-int idade = 32;
-double altura = 1.56;
-boolean matriculado = true;
-
-println($nome, )
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main (String[] args) {
+        String nome = "Núbia";
+        int idade = 32;
+        double altura = 1.56;
+        boolean matriculado = true;
+
+        System.out.println("Nome: " + nome + ", Idade: " + idade + ", Altura: " + altura + ", Matriculado: " + matriculado);
+    }
+}
+
